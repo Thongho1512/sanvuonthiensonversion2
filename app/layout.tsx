5369dacdbf31a610a1205425ffcebca357746c03
thongho <thongho1512@gmail.com>
2026-01-07 15:27:14 +0700

    update gmt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Script from "next/script"  // Thêm import Script từ next/script
 import { Cormorant_Garamond, Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
-import { GoogleTagManager } from "@next/third-parties/google"  // Thêm dòng này
 import { FloatingContact } from "@/components/floating-contact"
 import "./globals.css"
 
@@ -37,10 +37,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi" suppressHydrationWarning>
-      <head />
+      <head>
+        {/* Google Tag Manager - đặt trong <head> giống code mẫu */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TMCSL8G8');`,
+          }}
+        />
+      </head>
       <body className={`${cormorant.variable} ${inter.variable} font-sans antialiased`}>
-        <GoogleTagManager gtmId="GTM-TMCSL8G8" />
-
+        {/* Google Tag Manager (noscript) - đặt ngay sau opening <body> */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TMCSL8G8"
